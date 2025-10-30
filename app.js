31981a6169ef0e932c5579102b632ab055fc7d0a
9mz1 <muzyyyyyyy@gmail.com>
2025-10-30 22:48:30 +0400

    coded the API function

---
+++ app.js
@@ -0,0 +1,18 @@
+quoteText = document.querySelector("#quoteText");
+quoteBtn = document.querySelector("button");
+
+url = "https://api.kanye.rest/"
+
+function fetchQuote() {
+    fetch(url)
+    .then(response => response.json())
+    .then((json) => displayQuote(json.quote))
+    .catch((error) => console.error(error));
+};
+
+
+function displayQuote(quote){
+    quoteText.innerText = quote;
+};
+
+quoteBtn.addEventListener("click", fetchQuote);
